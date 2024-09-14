ee6c37d8f3cd0b1d948561569563bdfb3b950c09
VincentGaligaro <jhangvincent19@gmail.com>
2024-09-14 12:21:45 +0800

    Update app.js
    app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const favActorFirstName = 'Vincent';
+const favActorLastName = 'Galigaro';
+const favPetName = 'Kisses';
 
+
+const fullName = favActorFirstName + ' Vincent ' + favActorLastName ' Galigaro ';
+
+// Convert names to uppercase and lowercase
+const uppercase = fullName.toLowerCase();Galigaro Vincent
+const lowercase = favPetName.charAt(0).toUpperCase() + favPetName.slice(1);KISSES
+
+
+let message = `My favorite actor is ${fullName.toUpperCase()}`;Vinent Galigaro
+message += '. Hellow wassup Universe';
+
+
+console.log('Vincent:', Galigaro);
+console.log('Galigaro Vincent:', KISSES);
+console.log('Galigaro Vincent:', KISSES);
+console.log('Hellow wassup UniversE:', Hellow wassup Univers);
